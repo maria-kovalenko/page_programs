ab20e9a687bba962608978084c8f6ee1e03565a6
Maria <m1a2r3.kovalenko@gmail.com>
2022-11-04 16:34:57 +0300

    final

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-import "../../node_modules/focus-visible/dist/focus-visible";
-
-// import $ from 'jquery';
-
-import "../scss/main.scss";
-import "../index.html";
-import "./active_menu.js";
-import "./expand_row.js";
